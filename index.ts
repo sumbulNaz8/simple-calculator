48a4dd273036489f642ca604ec4cc8c43f585677
sunbulnaz <161361478+sunbulnaz@users.noreply.github.com>
2024-05-11 20:46:59 +0500

    tweak the text and added chalk function and add  operator Module,Exponents

--- index.ts
+++ index.ts
@@ -1,29 +1,63 @@
 #! /usr/bin/env node
+
+import chalk from 'chalk'
 import inquirer from "inquirer";
-console.log(`\n\t-----------------
+import Choices from 'inquirer/lib/objects/choices.js';
+console.log(chalk.green.italic`\n\t-----------------
                                     WELCOME TO MY SIMPLE CALCULATOR
                                                                         -----------------------------------\n`)
-
-const answer = await inquirer.prompt([
+                                                                     
+do{
+let answer = await inquirer.prompt([
   { message: "Enter your first number", type: "number", name: "firstNumber" },
   { message: "Enter your second number", type: "number", name: "secondNumber" },
   { message: "Select one of the operators to perform action",
     type: "list",
     name: "operator",
-    choices: ["Addition", "Subtraction", "Multiplication", "Division"],
-  },
-]);
+    choices: ["Addition", "Subtraction", "Multiplication", "Division" ,  "Modules" , "Exponents"],
+  }, 
 
-//conditional  ststement
+]);
+//conditional  statement
 if(answer. operator === "Addition"){  
-     console.log( answer.firstNumber + answer.secondNumber);
-}else if (answer. operator === "Subtraction"){  
-    console.log( answer.firstNumber - answer.secondNumber);
-}else if(answer. operator === "Multiplication"){  
-    console.log( answer.firstNumber * answer.secondNumber);
-}else if (answer. operator === "Division"){  
-    console.log( answer.firstNumber / answer.secondNumber);
+     console.log(chalk.green(answer.firstNumber + answer.secondNumber));
+}else if (answer.operator === "Subtraction") { 
+    console.log(chalk.magenta(answer.firstNumber - answer.secondNumber));
+}else if(answer.operator === "Multiplication"){  
+    console.log(chalk.red(answer.firstNumber * answer.secondNumber));
+}else if (answer.operator === "Division"){  
+    console.log(chalk.yellow(answer.firstNumber / answer.secondNumber));
+}else if (answer.operator === "Modules"){  
+  console.log(chalk.blue(answer.firstNumber % answer.secondNumber));
+}else if(answer.operator === "Exponents"){  
+  console.log(chalk.yellowBright(answer.firstNumber ** answer.secondNumber));
 }else{
-    console.log("Please select valid operator")
+  console.log(chalk.gray("please select valid operator"));
 }
-//********************************************** THE END ***************************************************/gi
+
+let ans =await inquirer . prompt([
+  {
+    name: 'continue',
+    type: 'confirm',
+    message: 'Do you want to perform another operation?',
+    default:true
+      
+  }
+]);
+if (ans.continue === false ){
+  console.log(chalk.cyan('you excited from calculator ...\n \t Goodbye have a nice day ...\n\t'));
+  break;
+}
+}while(true)
+
+
+
+
+console.log("***********************************THE END****************************************************")
+
+
+
+
+
+      
+       
